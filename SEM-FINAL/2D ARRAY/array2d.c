25424ee254533dcccb6b0cd1c645134c47101c98
Ismail Sabid <you@example.com>
2025-08-14 22:50:24 +0600

    input output

---
+++ SEM-FINAL/2D ARRAY/array2d.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+// //✌️prob1:taking input and printing elements;
+
+// int main(){
+//     int srow,scolumn;
+//     printf("Enter size of row and column sequentially= ");
+//     scanf("%d %d", &srow,&scolumn);
+//     int arr[srow][scolumn];
+// //taking input;
+//     for(int i=0;i<srow;i++){
+//         for(int j=0;j<scolumn;j++){
+//             printf("Enter element for (%d,%d) coordinate : ",i,j);
+//             scanf("%d",&arr[i][j]);
+//         }
+//     }
+// //printing the element:
+//     for(int i=0;i<srow;i++){
+//         for(int j=0;j<scolumn;j++){
+//             printf("Element for (%d,%d) coordinate : %d\n",i,j,arr[i][j]);
+            
+//         }
+//     }
+// }
